84ccf7437c8eabde8577c04cd589febc5433c362
FlakoBB <josephrbb99@gmail.com>
2023-08-01 18:55:59 -0600

    correcciones

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,41 +2,43 @@ import styles from '../styles/skills.module.css'
 
 const Skills = () => {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} shadow`}>
       <h3 className={styles.title}>Skills</h3>
       <table className={styles.tableSkills}>
-        <tr>
-          <td>JavaScript</td>
-          <td>
-            <div className={styles.progressBar}>
-              <div style={{ width: '80%' }} className={styles.progressBar__value} />
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>HTML</td>
-          <td>
-            <div className={styles.progressBar}>
-              <div style={{ width: '90%' }} className={styles.progressBar__value} />
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>CSS</td>
-          <td>
-            <div className={styles.progressBar}>
-              <div style={{ width: '80%' }} className={styles.progressBar__value} />
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>ReactJS</td>
-          <td>
-            <div className={styles.progressBar}>
-              <div style={{ width: '60%' }} className={styles.progressBar__value} />
-            </div>
-          </td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>JavaScript</td>
+            <td>
+              <div className={styles.progressBar}>
+                <div style={{ width: '80%' }} className={styles.progressBar__value} />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>HTML</td>
+            <td>
+              <div className={styles.progressBar}>
+                <div style={{ width: '90%' }} className={styles.progressBar__value} />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>CSS</td>
+            <td>
+              <div className={styles.progressBar}>
+                <div style={{ width: '80%' }} className={styles.progressBar__value} />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>ReactJS</td>
+            <td>
+              <div className={styles.progressBar}>
+                <div style={{ width: '60%' }} className={styles.progressBar__value} />
+              </div>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
