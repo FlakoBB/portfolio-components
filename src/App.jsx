485eb11c8b0323e47962c56a2310ee76a5b5a840
FlakoBB <josephrbb99@gmail.com>
2023-08-02 15:39:25 -0600

    ProjectsContainer completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import PersonalInformation from './components/PersonalInformation.jsx'
 import Project from './components/Project.jsx'
 import Skills from './components/Skills.jsx'
 import Hobbies from './components/Hobbies.jsx'
+import ProjectsContainer from './components/ProjectsContainer.jsx'
 
 const App = () => {
   return (
     <div className='mainContainer'>
+      <ProjectsContainer />
       <Hobbies />
       <Skills />
       <PersonalInformation />
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,28 @@
+import Project from './Project.jsx'
+import styles from '../styles/projectsContainer.module.css'
+
+const ProjectsContainer = () => {
+  return (
+    <section>
+      <header className={styles.header}>
+        <h2 className={styles.header__title}>Projects <span>(3)</span></h2>
+        <div className={styles.filter}>
+          {/* // ToDo: funcion que filtre proyectos y agregar la clase 'selected' cuando se seleccione el tag */}
+          <button className={`${styles.filter__tag} ${styles.selected}`}>React</button>
+          <button className={styles.filter__tag}>Django</button>
+          <button className={styles.filter__tag}>Vue</button>
+        </div>
+      </header>
+      <main className={styles.projectsList}>
+        {/* // ToDo: Renderizar proyectos cun map() de un arreglo */}
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+      </main>
+    </section>
+  )
+}
+export default ProjectsContainer
